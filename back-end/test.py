1c148c19863a049ab6393cb6d1c8b3627a0b054c
Jess DiPonzio <jsdiponzio@gmail.com>
2022-08-09 11:06:04 -0400

    Create tests for increment_fetch

---
+++ back-end/test.py
@@ -0,0 +1,55 @@
+import unittest
+from api import increment_fetch
+import mockfirestore
+
+class TestIncrementFetch(unittest.TestCase):
+
+	def test_count_not_null(self):
+		data = {
+			u'visitors': 3
+		}
+		mock_db = mockfirestore.MockFirestore()
+		mock_db.collection(u'visitors').document(u'count').set(data)
+		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
+		self.assertIsNotNone(updated_count)
+
+	def test_count_is_str(self):
+		data = {
+			u'visitors': 3
+		}
+		mock_db = mockfirestore.MockFirestore()
+		mock_db.collection(u'visitors').document(u'count').set(data)
+		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
+		self.assertIsInstance(updated_count, str)	
+
+	def test_count_increments(self):
+		data = {
+			u'visitors': 3
+		}
+		mock_db = mockfirestore.MockFirestore()
+		mock_db.collection(u'visitors').document(u'count').set(data)
+		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
+		self.assertEqual(int(updated_count), 4)
+	
+	def test_status_200(self):
+		data = {
+			u'visitors': 3
+		}
+		mock_db = mockfirestore.MockFirestore()
+		mock_db.collection(u'visitors').document(u'count').set(data)
+		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
+		self.assertEqual(status_code, 200)	
+
+	def test_headers_not_null(self):
+		data = {
+			u'visitors': 3
+		}
+		mock_db = mockfirestore.MockFirestore()
+		mock_db.collection(u'visitors').document(u'count').set(data)
+		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
+		self.assertIsNotNone(headers)	
+
+
+if __name__ == '__main__':
+  unittest.main()
+
