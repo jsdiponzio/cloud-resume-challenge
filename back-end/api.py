45f1da2f1e071a1512e568ca36f5ce4e2e37bbec
Jess DiPonzio <jsdiponzio@gmail.com>
2023-08-13 15:38:56 -0400

    Rename database fields

--- back-end/api.py
+++ back-end/api.py
@@ -5,12 +5,12 @@ def increment_fetch(request, test=False):
         db = firestore.Client(project='jess-cloud-resume-challenge')
     else:
         db = test
-    doc_ref = db.collection(u'visitors').document(u'count')
+    doc_ref = db.collection(u'site-views').document(u'visitors')
     doc = doc_ref.get()
-    count = doc.to_dict()['visitors']
+    count = doc.to_dict()['visitor-count']
 
     updated_count = count + 1
-    doc_ref.update({'visitors': updated_count})
+    doc_ref.update({'visitor-count': updated_count})
 
     headers = {
         'Access-Control-Allow-Origin': '*',
--- back-end/test.py
+++ back-end/test.py
@@ -6,46 +6,46 @@ class TestIncrementFetch(unittest.TestCase):
 
 	def test_count_not_null(self):
 		data = {
-			u'visitors': 3
+			u'visitor-count': 3
 		}
 		mock_db = mockfirestore.MockFirestore()
-		mock_db.collection(u'visitors').document(u'count').set(data)
+		mock_db.collection(u'site-views').document(u'visitors').set(data)
 		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
 		self.assertIsNotNone(updated_count)
 
 	def test_count_is_str(self):
 		data = {
-			u'visitors': 3
+			u'visitor-count': 3
 		}
 		mock_db = mockfirestore.MockFirestore()
-		mock_db.collection(u'visitors').document(u'count').set(data)
+		mock_db.collection(u'site-views').document(u'visitors').set(data)
 		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
 		self.assertIsInstance(updated_count, str)	
 
 	def test_count_increments(self):
 		data = {
-			u'visitors': 3
+			u'visitor-count': 3
 		}
 		mock_db = mockfirestore.MockFirestore()
-		mock_db.collection(u'visitors').document(u'count').set(data)
+		mock_db.collection(u'site-views').document(u'visitors').set(data)
 		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
 		self.assertEqual(int(updated_count), 4)
 	
 	def test_status_200(self):
 		data = {
-			u'visitors': 3
+			u'visitor-count': 3
 		}
 		mock_db = mockfirestore.MockFirestore()
-		mock_db.collection(u'visitors').document(u'count').set(data)
+		mock_db.collection(u'site-views').document(u'visitors').set(data)
 		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
 		self.assertEqual(status_code, 200)	
 
 	def test_headers_not_null(self):
 		data = {
-			u'visitors': 3
+			u'visitor-count': 3
 		}
 		mock_db = mockfirestore.MockFirestore()
-		mock_db.collection(u'visitors').document(u'count').set(data)
+		mock_db.collection(u'site-views').document(u'visitors').set(data)
 		updated_count, status_code, headers = increment_fetch(request="request", test=mock_db)
 		self.assertIsNotNone(headers)	
 
